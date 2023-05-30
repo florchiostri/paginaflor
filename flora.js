ba107154e14bb0bc583f9a4c51a3c0013b71128a
Flor Chiostri <130665833+florchiostri@users.noreply.github.com>
2023-05-30 04:11:21 -0300

    paginaflora
    flageventos

---
+++ flora.js
@@ -0,0 +1,8 @@
+var nombre= "Florencia"; //string
+var apellido= "Chiostri"; //string
+
+console.log(nombre+ " "+apellido);
+//resultado= Florencia Chiostri
+
+alert(console.log+ cliente)
+console.log(String+"cliente")
